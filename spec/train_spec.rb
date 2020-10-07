df0a55f9e76c74d7f0f701497c4d6a6bbc9b9340
Brycen Bartolome <brycenbartolome@gmail.com>
2020-10-07 13:45:37 -0700

    pass test, clears all trains
    Brian Harvey <brian.harv3y@gmail.com>

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -17,5 +17,17 @@ describe '#Train' do
       expect(Train.all).to(eq([train, train2]))
     end
   end
+  
+  describe('.clear') do
+    it('clears all trains.')do
+      train = Train.new({:color => "Blue", :id => nil})
+      train.save()
+      train2 = Train.new({:color => "Red", :id => nil})
+      train2.save()
+      Train.clear
+      expect(Train.all).to(eq([]))
+    end
+  end
+
 
 end
